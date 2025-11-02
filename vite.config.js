1633c610c6e7867d36e22829f7e05c3adc3b0361
dimaskendika <kendikad@gmail.com>
2025-11-02 12:47:30 +0700

    fix config

--- vite.config.js
+++ vite.config.js
@@ -23,16 +23,16 @@ export default defineConfig({
             },
         }),
     ],
-    server: {
-        host: '127.0.0.1',
-        port: 5173,
-        strictPort: true,
-        hmr: {
-            host: '127.0.0.1',
-            protocol: 'ws',
-            port: 5173,
-        },
-    },
+    // server: {
+    //     host: '127.0.0.1',
+    //     port: 5173,
+    //     strictPort: true,
+    //     hmr: {
+    //         host: '127.0.0.1',
+    //         protocol: 'ws',
+    //         port: 5173,
+    //     },
+    // },
     resolve: {
         alias: {
             "@": "/resources/js",
