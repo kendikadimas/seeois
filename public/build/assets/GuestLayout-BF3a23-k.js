1518df0ff0822d7ac01ae0a60c3dad482d387895
dimaskendika <kendikad@gmail.com>
2025-11-02 12:39:12 +0700

    Update React Vite build

---
+++ public/build/assets/GuestLayout-BF3a23-k.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as o,o as s,a,z as r}from"./app-UU5vQ_as.js";const c={},n={class:"bg-light",style:{"min-height":"100vh"}};function l(e,_){return s(),o("body",n,[a("main",null,[r(e.$slots,"default")])])}const d=t(c,[["render",l]]);export{d as G};
