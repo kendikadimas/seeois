047af47a5dc2705d830678c4c16fdc81583ae792
dimaskendika <kendikad@gmail.com>
2025-11-07 22:29:21 +0700

    fix: cast roles_id to integer and move certificates routes inside staff middleware group

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,6 +56,7 @@ class User extends Authenticatable implements MustVerifyEmail
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'roles_id' => 'integer',  // Force roles_id to always be integer
         ];
     }
 
--- routes/web.php
+++ routes/web.php
@@ -232,6 +232,7 @@ Route::middleware(['auth', 'verified', 'staff'])->group(function () {
         Route::get('/internship/certificate/download/{id}', [InternshipCertificateController::class, 'download'])->name('certificate.download');
     });
 
+    // Internship Certificates Management - For Staff Only (HR Manager & PIC Internship)
     Route::prefix('staff')->group(function(){
         Route::get('/internship/certificates/manage', [InternshipCertificateController::class, 'manageIndex'])->name('certificate.manage');
         Route::post('/internship/certificate/store', [InternshipCertificateController::class, 'store'])->name('certificate.store');
